fb3bcba5da2f82ce3685e76249871898c73dc176
Sercan <s.selcuk23@gmail.com>
2021-06-30 00:53:59 +0300

    Automap is added

---
+++ ShoptionMicroservices/Services/Shoption.Catalog/Mapping/MapFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AutoMapper;
+using Shoption.Catalog.Dto;
+using Shoption.Catalog.Models;
+
+namespace Shoption.Catalog.Mapping
+{
+    public class MapFactory:Profile
+    {
+        public MapFactory()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+
+            CreateMap<Product, ProductCreateDto>().ReverseMap();
+            CreateMap<Product, ProductUpdateDto>().ReverseMap();
+        }   
+    }
+}
