5d33e37a96bcecee76a89831d6c74f01dcd4de15
Sercan <s.selcuk23@gmail.com>
2021-06-30 22:15:58 +0300

    User signup is ready

---
+++ ShoptionMicroservices/IdentityServer/Shoption.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shoption.IdentityServer.Dtos;
+using Shoption.IdentityServer.Models;
+using Shoption.Shared.Dto;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Shoption.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.Username,
+                Email = signupDto.Email,
+                City = signupDto.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+
+            return NoContent();
+        }
+    }
+}
---
+++ ShoptionMicroservices/IdentityServer/Shoption.IdentityServer/Dtos/SignupDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Shoption.IdentityServer.Dtos
+{
+    public class SignupDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+    }
+}
