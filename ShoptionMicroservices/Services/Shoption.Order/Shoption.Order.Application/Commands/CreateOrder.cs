64767e40ee88d6c0f88ec3526f29eee53c1a1e95
Sercan <s.selcuk23@gmail.com>
2021-07-06 00:05:28 +0300

    Command and Queries are ready

---
+++ ShoptionMicroservices/Services/Shoption.Order/Shoption.Order.Application/Commands/CreateOrder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Shoption.Order.Application.Dtos;
+using Shoption.Order.Domain.OrderAggregate;
+using Shoption.Order.Infrastructure;
+using Shoption.Shared.Dto;
+
+namespace Shoption.Order.Application.Commands
+{
+    public class CreateOrder : IRequest<Response<CreatedOrderDto>>
+    {
+        public string BuyerId { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+        public AddressDto Address { get; set; }
+    }
+
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrder, Response<CreatedOrderDto>>
+    {
+        private readonly OrderDbContext _context;
+
+        public CreateOrderCommandHandler(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        async Task<Response<CreatedOrderDto>> IRequestHandler<CreateOrder, Response<CreatedOrderDto>>.Handle(CreateOrder request, CancellationToken cancellationToken)
+        {
+            var newAddress = new Address(
+                request.Address.District,
+                request.Address.Line,
+                request.Address.Province,
+                request.Address.Street,
+                request.Address.ZipCode
+            );
+
+            Domain.OrderAggregate.Order newOrder = new Domain.OrderAggregate.Order(newAddress, request.BuyerId);
+
+            request.OrderItems.ForEach(x =>
+            {
+                newOrder.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureUrl);
+            });
+
+            await _context.Orders.AddAsync(newOrder);
+
+            await _context.SaveChangesAsync();
+
+            return Response<CreatedOrderDto>.Success(new CreatedOrderDto { OderId = newOrder.Id }, 200);
+        }
+    }
+}
---
+++ ShoptionMicroservices/Services/Shoption.Order/Shoption.Order.Application/Queries/GetOrderByUserId.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shoption.Order.Application.Dtos;
+using Shoption.Order.Application.Mapping;
+using Shoption.Order.Infrastructure;
+using Shoption.Shared.Dto;
+
+namespace Shoption.Order.Application.Queries
+{
+    public class GetOrderByUserId : IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+
+    public class GetOderByUserIdHandler : IRequestHandler<GetOrderByUserId, Response<List<OrderDto>>>
+    {
+        private readonly OrderDbContext _orderDbContext;
+
+        public GetOderByUserIdHandler(OrderDbContext orderDbContext)
+        {
+            _orderDbContext = orderDbContext;
+        }
+
+        public async Task<Response<List<OrderDto>>> Handle(GetOrderByUserId request, CancellationToken cancellationToken)
+        {
+            var orders = await _orderDbContext.Orders.Include(x => x.OrderItem).Where(x => x.BuyerId == request.UserId).ToListAsync();
+
+            if (!orders.Any())
+                return Response<List<OrderDto>>.Success(new List<OrderDto>(), 200);
+
+            var ordersDto = ObjectMapper.Mapper.Map<List<OrderDto>>(orders);
+
+            return Response<List<OrderDto>>.Success(ordersDto, 200);
+        }
+    }
+}
